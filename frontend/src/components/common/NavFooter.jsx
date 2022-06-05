5e19d8cca2b429eaf3a4ffc78311043eb5fcead7
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-06-04 21:51:45 -0400

    push the update, without transaction yet

--- frontend/src/components/common/NavFooter.jsx
+++ frontend/src/components/common/NavFooter.jsx
@@ -1,24 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux';
-// import { setTheme } from '../../features/local/localSlice';
-// import { Switch } from '../';
+import { setTheme } from '../../features/local/localSlice';
+import { Switch } from '../';
 
 
 const NavFooter = () => {
-    // const dispatch = useDispatch();
-    // const theme = useSelector((state) => state.local.theme);
+    const dispatch = useDispatch();
+    const theme = useSelector((state) => state.local.theme);
     const ethPrice = useSelector((state) => state.local.ethPrice);
 
     return (
         <div>
-            <div className="p-3 flex align-between border">
+            {/* <div className="p-3 flex align-between border">
                 <div className="title-3">
                     ETH Price:
                 </div>
                 <div className="title-3">
                     {ethPrice.toFixed(2)} USD
                 </div>
-            </div>
-            {/* <div className="p-1 flex align-between border-top">
+            </div> */}
+            <div className="p-1 flex align-between border-top">
                 <div className="title-3">
                     Dark Theme
                 </div>
@@ -30,7 +30,7 @@ const NavFooter = () => {
                 >
                     mode
                 </Switch>
-            </div> */}
+            </div>
         </div>
     )
 }
